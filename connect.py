cf48947ed4be7aeb26b6e270e52a805cd84912da
MichalKalmucki <kalmuckimichal@gmail.com>
2024-06-12 23:49:11 +0200

    test connect michal

--- connect.py
+++ connect.py
@@ -1,33 +1,46 @@
 from cassandra.cluster import Cluster
-from cassandra.query import BatchStatement
 
 
-class Connect:
-    def __init__(self):
-        self.cluster = Cluster(
-            contact_points=[
-            ('127.0.0.1', 9042),
-            ('127.0.0.1', 9043),
-            ('127.0.0.1', 9044)
-            ]
-        )
-        self.connect('flights_system')
+# class Connect:
+#     def __init__(self):
+#         self.cluster = Cluster(
+#             contact_points=[
+#             ('127.0.0.1', 9042),
+#             ('127.0.0.1', 9043),
+#             ('127.0.0.1', 9044)
+#             ]
+#         )
+#         self.connect('flights_system')
 
-    def connect(self, keyspace_name):
-        metadata = self.cluster.metadata
-        if keyspace_name not in metadata.keyspaces:
-            keyspace_name = None
-        self.session = self.cluster.connect(keyspace_name)
+#     def connect(self, keyspace_name):
+#         metadata = self.cluster.metadata
+#         if keyspace_name not in metadata.keyspaces:
+#             keyspace_name = None
+#         self.session = self.cluster.connect(keyspace_name)
 
-    def execute(self, query):
-        return self.session.execute(query)
+#     def execute(self, query):
+#         return self.session.execute(query)
     
-    def get_cluster(self):
-        return self.cluster
+#     def get_cluster(self):
+#         return self.cluster
 
-    def get_session(self):
-        return self.session
+#     def get_session(self):
+#         return self.session
     
-    def close(self):
-        self.cluster.shutdown()
+#     def close(self):
+#         self.cluster.shutdown()
     
+
+cluster = Cluster(
+    contact_points=[
+      ('127.0.0.1', 9042),
+      ('127.0.0.1', 9043)
+    ]
+)
+
+session = cluster.connect()
+
+query = "CREATE KEYSPACE IF NOT EXISTS airline WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }"
+session.execute(query)
+
+session.set_keyspace('airline')
--- test.py
+++ test.py
@@ -1,15 +1,7 @@
-from cassandra.cluster import Cluster
-from cassandra.query import ConsistencyLevel
-from datetime import datetime
 from services.BookingService import BookingService
-import unittest
-
-cluster = Cluster(
-    contact_points=[
-      ('127.0.0.1', 9042),
-      ('127.0.0.1', 9043)
-    ]
-)
+from services.FlightService import FlightService
+from datetime import datetime
+from connect import session
 
 
 # class CassandraTests(unittest.TestCase):
@@ -17,10 +9,13 @@ cluster = Cluster(
 #         self.assertEqual(session.keyspace, 'flights_system')
 
 
-
 if __name__ == '__main__':
     BookingService = BookingService()
-    flight_id = BookingService.create_initial()
-    BookingService.book_tickets(flight_id, 2, ['4Mike', '4Jef'])
-    BookingService.display_bookings(flight_id)
-    # BookingService.remove_booking("aca51eec-0766-49c7-adc5-c1a5b5fe3b4a")
+    FlightService = FlightService()
+    # flight_id = BookingService.create_initial()
+    # BookingService.addBooking(flight_id, 8, ['1Mike', '2Jef', '3John', '4Doe', '5Jane', '6Ron', '7Harry', '8Potter'])
+    # BookingService.getAllBookingsById(flight_id)
+    # FlightService.addFlight('JFK', 'LAX', datetime.now(), datetime.now())
+    # BookingService.updateBooking('b2009b11-371c-4f0c-a494-506603d719bd', '5db5b464-905f-4e34-8fda-2e53581a1295')
+    BookingService.addBooking('120fc823-97af-4717-a0f9-6417c7129739', 1, ['1Mike'])
+    FlightService.getAllFlights()
